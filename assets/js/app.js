8c7f7b8bead52e736c2fabd53c09405e4647a08f
div3r <div3r.zg@gmail.com>
2019-04-04 15:40:43 +0200

    added trumbowyg & missing icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,19 @@
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 
+require('trumbowyg');
+require('trumbowyg/dist/ui/trumbowyg.min.css');
+
+
 // any CSS you require will output into a single css file (app.scss in this case)
 require('../css/app.scss');
 
 const $ = require('jquery');
 window.jQuery = $;
 window.Popper = require('popper.js');
+
+$('.mojID').trumbowyg({
+    svgPath: false
+});
+
+$.trumbowyg.svgPath = '/icons.svg';
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -9,17 +9,34 @@
 namespace App\Controller;
 
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
+    private $articleRepository;
+
+    /**
+     * HomepageController constructor.
+     * @param ArticleRepository $ar
+     */
+    public function __construct(ArticleRepository $ar)
+    {
+        $this->articleRepository = $ar;
+    }
+
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('homepage/index.html.twig');
+
+        $articles = $this->articleRepository->findAll();
+        return $this->render('homepage/index.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
 
